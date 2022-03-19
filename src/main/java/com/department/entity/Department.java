f92c9e30ae404fcccdee0f3986d00c2c48331b18
Ingsum <meetgajen@outlook.com>
2022-03-19 13:21:17 -0500

    load

---
+++ src/main/java/com/department/entity/Department.java
@@ -0,0 +1,25 @@
+package com.department.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="departments")
+public class Department {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long departmentId;
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
+
+}
